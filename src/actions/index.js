f5e52c71dd59a4205a86d99cb575051cb54b52ed
boyboi86 <boyboi86@gmail.com>
2016-09-05 14:19:58 +0800

    added action creators

---
+++ src/actions/index.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+const ALL_TIME_URL = 'https://fcctop100.herokuapp.com/api/fccusers/top/alltime';
+const RECENT_URL = 'https://fcctop100.herokuapp.com/api/fccusers/top/recent';
+
+export ALL_TIME_FETCH = 'ALL_TIME_FETCH';
+export RECENT_FETCH = 'RECENT_FETCH';
+
+export function alltimeFetch(){
+
+  const request = axios.get(ALL_TIME_URL);
+
+  return {
+    type: ALL_TIME_FETCH,
+    payload: request
+  };
+}
+
+
+export function recentFetch(){
+
+  const request = axios.get(RECENT_URL);
+
+  return {
+    type: RECENT_FETCH,
+    payload: request
+  };
+}
