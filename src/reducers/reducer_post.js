d76e8b06b38f1ec6445850d5ed8df6512868d1a0
boyboi86 <boyboi86@gmail.com>
2016-09-05 15:01:34 +0800

    sub-reducer creation

--- src/reducers/reducer_post.js
+++ src/reducers/reducer_post.js
@@ -1,11 +1,11 @@
 import { ALL_TIME_FETCH, RECENT_FETCH } from '../actions/index';
 
-export default function(state, action){
+export default function(state = {post: null}, action){
   switch (action.type) {
     case ALL_TIME_FETCH:
-      return {...state, action.payload.data};
+      return {...state, post: action.payload.data};
     case RECENT_FETCH:
-      return {...state, action.payload.data};
+      return {...state, post: action.payload.data};
     default:
     return state;
   }
