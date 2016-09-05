afb442edb933505767a29ebd56f8a5e552a75ec7
boyboi86 <boyboi86@gmail.com>
2016-09-05 16:46:09 +0800

    update action creator and connect

--- src/components/recenttimes.js
+++ src/components/recenttimes.js
@@ -1,7 +1,26 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
+import { recentFetch } from '../actions/index';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 
-export default class RECENT_Times extends Component{
+class RECENT_Times extends Component{
+  componentWillMount(){
+    this.props.recentFetch();
+  }
+
   render(){
-    return <div>duck</div>
+    return (
+      <div>
+        <Link to="/" className="btn btn-warning pull-xs-right">Back to top scores</Link>
+        <h3>Recent high scores</h3>
+      </div>
+    )
   }
 }
+
+function mapDispatchToProps(dispatch){
+  return bindActionCreators({ recentFetch }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(RECENT_Times);
