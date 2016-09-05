66804b8b8a4a91e0372317501eb506e5ec630ac7
boyboi86 <boyboi86@gmail.com>
2016-09-05 16:27:58 +0800

    change state to array

--- src/reducers/reducer_post.js
+++ src/reducers/reducer_post.js
@@ -1,6 +1,8 @@
 import { ALL_TIME_FETCH, RECENT_FETCH } from '../actions/index';
 
-export default function(state = {post: null}, action){
+const INITIAL_STATE = { post: [] }
+
+export default function(state = INITIAL_STATE, action){
   switch (action.type) {
     case ALL_TIME_FETCH:
       return {...state, post: action.payload.data};
