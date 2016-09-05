28fab08bbc8d9d32b86efc4c84300f290fbb0786
boyboi86 <boyboi86@gmail.com>
2016-09-05 17:02:09 +0800

    amend on initial state

--- src/reducers/reducer_post.js
+++ src/reducers/reducer_post.js
@@ -1,13 +1,13 @@
 import { ALL_TIME_FETCH, RECENT_FETCH } from '../actions/index';
 
-const INITIAL_STATE = { post: [] }
+const INITIAL_STATE = { Apost: [], Rpost: [] }
 
 export default function(state = INITIAL_STATE, action){
   switch (action.type) {
     case ALL_TIME_FETCH:
-      return {...state, post: action.payload.data};
+      return {...state, Apost: action.payload.data};
     case RECENT_FETCH:
-      return {...state, post: action.payload.data};
+      return {...state, Rpost: action.payload.data};
     default:
     return state;
   }
