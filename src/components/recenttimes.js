ee00dbe892fc21d824d5df6e948140d62fbff01d
boyboi86 <boyboi86@gmail.com>
2016-09-05 16:28:41 +0800

    added new route

---
+++ src/components/recenttimes.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class RECENT_Times extends Component{
+  render(){
+    return <div>duck</div>
+  }
+}
--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,11 @@ import { Router, Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
 import ALL_Times from './components/alltimes';
+import RECENT_Times from './components/recenttimes';
 
 export default (
   <Router path="/" component={ App }>
     <IndexRoute component={ ALL_Times }/>
+    <Route path="/recent" component={ RECENT_Times } />
   </Router>
 )
