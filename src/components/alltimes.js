46466f8447381fb629bd0d0bd2fc21c283a46721
boyboi86 <boyboi86@gmail.com>
2016-09-05 15:00:46 +0800

    connect reducer && dispatch

--- src/components/alltimes.js
+++ src/components/alltimes.js
@@ -1,7 +1,28 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux';
+import { alltimeFetch } from '../actions/index';
 
-export default class ALL_Times extends Component{
+class ALL_Times extends Component{
+  componentWillMount(){
+    this.props.alltimeFetch();
+  }
+
   render(){
-    return <div>Chicken</div>
+    return this.props.posts.map((el, index) => {
+      return (
+        <div>{el.username}</div>
+      )
+    })
   }
 }
+
+function mapDispatchToProps(dispatch){
+  return bindActionCreators({ alltimeFetch }, dispatch);
+}
+
+function mapStateToProps(state){
+  return { posts: state.posts.post }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ALL_Times);
